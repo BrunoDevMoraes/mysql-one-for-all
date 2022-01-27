b3493386fa3cefe87ce8ab40bec6e53e5b999456
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-01-27 20:11:27 -0300

    Add 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+    a.artista_nome AS artista,
+    b.album_nome AS album,
+    COUNT(c.artista_id) AS seguidores
+FROM
+    SpotifyClone.artistas AS a
+        INNER JOIN
+    SpotifyClone.albuns AS b ON a.artista_id = b.artista_id
+        INNER JOIN
+    SpotifyClone.usuario_artista AS c ON a.artista_id = c.artista_id
+GROUP BY artista, album
+ORDER BY seguidores DESC;
