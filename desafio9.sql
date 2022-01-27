73432499b600a9543fa9a93fff9128df7a886395
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-01-27 20:31:49 -0300

    Add 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT
+    COUNT(a.usuario_id) AS quantidade_musicas_no_historico
+FROM
+    SpotifyClone.usuario_musica AS a
+        INNER JOIN
+    SpotifyClone.usuarios AS c ON c.usuario_id = a.usuario_id AND c.usuario_nome = 'Bill'
