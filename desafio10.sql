81769285f3487b26e4b6ce9ee3cc60dd1ab91c7d
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-01-28 01:13:27 -0300

    Fix 10 i

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,12 @@
 SELECT
-	c.musica_nome AS nome,
-    COUNT(a.musica_id) AS reproducoes
+    c.musica_nome AS nome, COUNT(a.musica_id) AS reproducoes
 FROM
-	SpotifyClone.musicas AS c
-   	INNER JOIN SpotifyClone.usuario_musica AS a ON a.musica_id = c.musica_id
-   	INNER JOIN SpotifyClone.usuarios AS b ON b.usuario_id = a.usuario_id
+    SpotifyClone.musicas AS c
+        INNER JOIN
+    SpotifyClone.usuario_musica AS a ON a.musica_id = c.musica_id
+        INNER JOIN
+    SpotifyClone.usuarios AS b ON b.usuario_id = a.usuario_id
 WHERE
-	b.plano_id IN (1, 4)
-GROUP BY
-	nome
-ORDER BY
-	nome;
+    b.plano_id IN (1 , 4)
+GROUP BY nome
+ORDER BY nome;
