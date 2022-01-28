d4e252a2d427c6564634650a8a354f8aa52e2c17
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-01-28 00:32:29 -0300

    Fix 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,12 @@
 SELECT
-    a.usuario_nome AS usuarios,
+    a.usuario_nome AS usuario,
     COUNT(c.usuario_id) AS qtde_musicas_ouvidas,
-    ROUND((SUM(b.duracao) / 60), 2) AS total_minutos
+    ROUND((SUM(b.duracao))/ 60, 2) AS total_minutos
 FROM
     SpotifyClone.usuarios AS a
         INNER JOIN
     SpotifyClone.usuario_musica AS c ON a.usuario_id = c.usuario_id
         INNER JOIN
-    SpotifyClone.musicas AS b ON c.musica_id = b.musica_id
-GROUP BY a.usuario_nome
+    SpotifyClone.musicas AS b ON b.musica_id = c.musica_id
+GROUP BY usuario
 ORDER BY a.usuario_nome;
