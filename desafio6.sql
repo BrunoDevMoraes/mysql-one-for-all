06c909e1e093a419749c9f1d2b1928cd1d16373a
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-01-28 00:40:05 -0300

    Fix 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,9 @@
 SELECT
-    ROUND(MIN(a.plano_valor), 2) AS faturamento_minimo,
+    MIN(a.plano_valor) AS faturamento_minimo,
     ROUND(MAX(a.plano_valor), 2) AS faturamento_maximo,
     ROUND(AVG(a.plano_valor), 2) AS faturamento_medio,
     ROUND(SUM(a.plano_valor), 2) AS faturamento_total
 FROM
-    SpotifyClone.planos AS a INNER JOIN SpotifyClone.usuarios as c ON a.plano_id = c.plano_id;
+    SpotifyClone.usuarios AS c
+        INNER JOIN
+    SpotifyClone.planos AS a ON a.plano_id = c.plano_id;
